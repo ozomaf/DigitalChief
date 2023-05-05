b2f93f96b92cac92ebfe9ac38f942fecd4abdeb4
azatkhaliyllin <khaliyllin56@gmail.com>
2023-05-05 19:03:46 +0400

    initial commit

---
+++ src/main/java/com/azatkhaliullin/digitalchief/DigitalChiefApplication.java
@@ -0,0 +1,13 @@
+package com.azatkhaliullin.digitalchief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalChiefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azatkhaliullin/digitalchief/DigitalChiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azatkhaliullin.digitalchief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
